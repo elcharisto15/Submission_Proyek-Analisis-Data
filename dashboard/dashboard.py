f163567930fd3f15ca03b4b143a2a926a85b1468
elcharisto15 <137696764+elcharisto15@users.noreply.github.com>
2024-10-06 13:17:49 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -25,21 +25,21 @@ end_date = pd.to_datetime(end_date)
 filtered_day_df = day_df[(day_df['dteday'] >= start_date) & (day_df['dteday'] <= end_date)]
 filtered_hour_df = hour_df[(hour_df['dteday'] >= start_date) & (hour_df['dteday'] <= end_date)]
 
-
-# Jumlah Sewa per hari
-st.subheader('Jumlah Sewa Per Hari')
-plt.figure(figsize=(12, 6))
-plt.plot(filtered_day_df['dteday'], filtered_day_df['cnt'])
-plt.xlabel('Tanggal')
-plt.ylabel('Jumlah Sewa')
-plt.xticks(rotation=45)
-st.pyplot(plt)
-
 # Rata-rata Jumlah Sewa Berdasarkan Kondisi Cuaca
 st.subheader('Rata-rata Jumlah Sewa Berdasarkan Kondisi Cuaca')
 plt.figure(figsize=(10, 6))
-sns.barplot(x='weathersit', y='cnt', data=filtered_hour_df)
+sns.barplot(x='weathersit', y='cnt', data=filtered_hour_df, color='yellow')
 plt.xlabel('Kondisi Cuaca (1: Cerah, 2: Berkabut, 3: Hujan Ringan, 4: Hujan Lebat)')
 plt.ylabel('Jumlah Sewa')
 st.pyplot(plt)
 
+# Rata-rata Jumlah Sewa Berdasarkan Bulan
+st.subheader('Rata-rata Jumlah Sewa tiap bulan')
+perbulan = filtered_day_df.groupby('mnth')['cnt'].mean().reset_index()
+bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+perbulan['mnth'] = perbulan['mnth'].apply(lambda x: bulan[x-1])
+plt.figure(figsize=(10, 5))
+sns.barplot(data=perbulan, x='mnth', y='cnt')
+plt.xlabel('Bulan')
+plt.ylabel('Jumlah Rata-rata Sewa Sepeda')
+st.pyplot(plt)
